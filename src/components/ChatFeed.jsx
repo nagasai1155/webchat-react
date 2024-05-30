f5a40f19a3987271f66e780345ddf8408baefc84
nagasai1155 <nagasaibalam@gmail.com>
2024-05-30 16:43:21 +0530

    chat feed

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,17 @@
+import Message from "./Message";
+import Mymessage from "./Mymessage";
+import Theirmessage from "./Theirmessage";
+
+
 const ChatFeed = (props)=>{
- console.log(props);
+  const {chats,activeChat,userName,messages} = props;
+  const chat = chats && chats[activeChat];
+  console.log(chat,userName,messages);
+  const renderMessages=()=>{
+    const keys = Object.keys(messages);
+    console.log(keys);
+  }
+  renderMessages();
  return(
     <>
     <h3>chatfeed</h3>
